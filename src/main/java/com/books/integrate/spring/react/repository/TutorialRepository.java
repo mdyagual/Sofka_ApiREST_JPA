d4e25116e8d8e75829d74e96266851878650df42
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-18 23:11:05 -0500

    ÂCambio en definición de funciones provistas por JPA

--- src/main/java/com/books/integrate/spring/react/repository/TutorialRepository.java
+++ src/main/java/com/books/integrate/spring/react/repository/TutorialRepository.java
@@ -1,11 +1,14 @@
 package com.books.integrate.spring.react.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.books.integrate.spring.react.model.Tutorial;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
 	List<Tutorial> findByPublished(boolean published);
-	List<Tutorial> findByTitleContaining(String title);
+	//List<Tutorial> findByTitleContaining(String title);
+
+	List<Tutorial> findByTitle(String title);
 }
